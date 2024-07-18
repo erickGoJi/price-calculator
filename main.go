17f62ce194543467f700add87108cfafe2710cb2
erickGoJi <erickgomezlol96@gmail.com>
2024-07-17 19:56:09 -0600

    chore: Refactor TaxIncludedPriceJob constructor to use InputPrices instead of Prices

--- main.go
+++ main.go
@@ -1,26 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"example.com/price-calculator/prices"
+)
 
 // Function Price calculates the price of a product with tax
 func main() {
-	prices := []float64{10, 20, 30}
+	// prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
-	// Calculate the price of a product with tax
 	for _, taxRate := range taxRates {
-		// Create a slice to store the prices with tax
-		var taxIncludedPrices []float64 = make([]float64, len(prices))
-		// Calculate the price of a product with tax
-		for priceIndex, price := range prices {
-			// Calculate the price of a product with tax
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		// Store the prices with tax in the result map
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,26 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
 
+// Process
+func (j TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+	// Calculate the price of a product with tax
+	for _, price := range j.InputPrices {
+		// Calculate the price of a product with tax
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + j.TaxRate)
+	}
+
+	fmt.Println(result)
+}
+
+// NewTaxIncludedPriceJob creates a new TaxIncludedPriceJob
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
 		InputPrices: []float64{10, 20, 30},
