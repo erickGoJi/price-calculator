fcba20a792eaf290e55a8bc7601e3b3a07ad810b
erickGoJi <erickgomezlol96@gmail.com>
2024-07-28 13:06:36 -0600

    refactor: Update FileManager methods to use struct receiver instead of function

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	"example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
 )
 
@@ -10,7 +13,8 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("output_%v.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		priceJob.Process()
 	}
 
--- prices/prices.go
+++ prices/prices.go
@@ -8,13 +8,14 @@ import (
 )
 
 type TaxIncludedPriceJob struct {
+	IOManager         filemanager.FileManager
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]string
 }
 
 func (j *TaxIncludedPriceJob) LoadData() {
-	lines, err := filemanager.ReadLines("prices.txt")
+	lines, err := j.IOManager.ReadLines()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -43,12 +44,13 @@ func (j *TaxIncludedPriceJob) Process() {
 
 	j.TaxIncludedPrices = result
 
-	filemanager.WriteJSON(fmt.Sprintf("result_%.0f.json", j.TaxRate*100), j)
+	j.IOManager.WriteJSON(j)
 }
 
 // NewTaxIncludedPriceJob creates a new TaxIncludedPriceJob
-func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
+		IOManager:   fm,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
